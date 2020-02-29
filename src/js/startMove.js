3096ab3ef155dca20fb55d04ee27a2ca938b8eaa
sao-lang <3089549839@qq.com>
2020-02-29 11:48:46 +0800

    the second commit at my project

---
+++ src/js/startMove.js
@@ -0,0 +1,37 @@
+var flag = null;
+
+function startMove(obj, json, time, callback) {
+    flag = true;
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+        for (var attr in json) {
+            var current = 0;
+            if (attr == "opacity") {
+                current = parseFloat(getStyle(obj, attr)) * 100;
+            } else if (attr == "zIndex") {
+                current = parseFloat(getStyle(obj, attr)) * 100;
+            } else {
+                current = parseInt(getStyle(obj, attr));
+            }
+            var speed = (json[attr] - current) / 10;
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+            if (json[attr] != current) {
+                flag = false;
+            }
+            if (attr == "opacity") {
+                obj.style.opacity = (current + speed) / 100;
+            } else if (attr == "zIndex") {
+                obj.style.zIndex = json[attr];
+            } else {
+                obj.style[attr] = current + speed + "px";
+            }
+        }
+        if (flag) {
+            clearInterval(obj.timer);
+            if (callback) {
+                callback();
+            }
+        }
+
+    }, time)
+}
